e3938cd79ae17ca5d3351efb79208da19c4acbda
wenzhiquan <wenzhiquanr@163.com>
2015-08-05 09:26:42 +0800

    完成browsersync的实时刷新前台界面

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,26 @@
 var gulp = require('gulp'),
-    gulpLoadPlugins = require('gulp-load-plugins'),
-    plugins = gulpLoadPlugins(),
-    browserSync = require('browser-sync');
+  gulpLoadPlugins = require('gulp-load-plugins'),
+  plugins = gulpLoadPlugins(),
+  browserSync = require('browser-sync').create();
 
-gulp.task('sync', function () {
-   var files = [
-      'public/javascripts/**/*.js'
-   ];
+gulp.task('server', ['js'], function() {
+  var files = [
+    'public/javascripts/**/*.js'
+  ];
 
-   browserSync.init(files, {
-      server: {
-         baseDir: './public'
-      }
-   });
+  browserSync.init(files, {
+    proxy: "localhost:3000"
+  });
 
-   gulp.watch(files).on('change', browserSync.reload);
+  gulp.watch(files, ['js']);
+  gulp.watch(files).on('change', browserSync.reload);
 });
 
-gulp.task('js', function () {
-   return gulp.src(['public/javascripts/*.js'])
-      .pipe(plugins.jshint())
-      .pipe(plugins.jshint.reporter('default'))
-      .pipe(plugins.uglify())
-      .pipe(plugins.concat('build.js'))
-      .pipe(gulp.dest('public/javascripts/build'))
-});
-
-gulp.task('watch', function() {
-    // app/**/*.*的意思是 app文件夹下的 任何文件夹 的 任何文件
-    gulp.watch('public/javascripts/*.js', ['js']);
+gulp.task('js', function() {
+  return gulp.src(['public/javascripts/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('default'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('build.js'))
+    .pipe(gulp.dest('public/javascripts/build'))
 });
--- public/javascripts/MyController.js
+++ public/javascripts/MyController.js
@@ -1,16 +1,21 @@
 var app = angular.module('app', []);
 
-var MyController = function ($scope, $timeout) {
-    var updateClock = function() {
-        $scope.clock = new Date();
-        $timeout(function() {
-            updateClock();
-        }, 1000);
+var MyController = function($scope) {
+    $scope.clock = {
+        now: new Date()
     };
+
+    var updateClock = function() {
+        $scope.clock.now = new Date();
+    };
+
+    setInterval(function() {
+        $scope.$apply(updateClock);
+    }, 1000);
 
     updateClock();
 };
 
-MyController.$inject = ['$scope', '$timeout'];
+MyController.$inject = ['$scope'];
 
 app.controller("MyController", MyController);
